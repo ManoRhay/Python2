dc57d4edd887a8b2c255b1fb7d93c9cfb44c1c38
Rhayan Adriell <rhayanadriell57@outlook.com>
2024-08-12 19:14:35 -0300

    KickTarefas

---
+++ tarefinhas.py
@@ -0,0 +1,71 @@
+#TAREFA 1
+# nota1=float(input("Digite a primeira nota: "))
+# nota2=float(input("Digite a segunda nota: "))
+# nota3=float(input("Digite a terceira nota: "))
+
+# media = (nota1 + nota2 + nota3)/3
+
+# print("A média final é: ",media)
+
+
+
+#TAREFA 2
+# peso = float(input("Digite seu peso em kg: "))
+# altura = float(input("Digite sua altura em metros: "))
+
+# imc = peso / (altura ** 2)
+
+# if imc < 18.5:
+#     classificacao = "Abaixo do peso"
+# elif imc < 24.9:
+#     classificacao = "Peso normal"
+# elif imc < 29.9:
+#     classificacao = "Sobrepeso"
+# elif imc < 34.9:
+#     classificacao = "Obesidade Grau I"
+# elif imc < 39.9:
+#     classificacao = "Obesidade Grau II"
+# else:
+#     classificacao = "Obesidade Grau III (Mórbida)"
+
+# print(f"Seu IMC é: {imc:.2f}")
+# print(f"Classificação: {classificacao}")
+
+
+
+# #TAREFA 3
+# for _ in range(5):
+#     print("Hello World")
+
+
+
+# #TAREFA 4
+# for i in range(1, 151):
+#     print(i)
+
+
+
+# #TAREFA 5
+# for i in range(10, -1, -1):
+#     print(i)
+
+
+
+# #TAREFA 6
+# for i in range(2, 201, 2):
+#     print(i)
+
+
+
+# #TAREFA 7
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i * j}")
+#     print()  # Separa as tabuadas com uma linha em branco. Só por pura organização, espero que nao se incomode ksks
+
+
+
+# #TAREFA 8
+# n = int(input("Digite um número inteiro: "))
+# for i in range(1, n + 1, 2):
+#     print(i)
